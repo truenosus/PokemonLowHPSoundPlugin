715b82d15579507ec723fdf0938a98797356eba5
NoahO <81601022+truenosus@users.noreply.github.com>
2023-03-08 08:43:25 -0500

    done

--- src/test/java/com/pokemonsound/PokemonSoundPluginTest.java
+++ src/test/java/com/pokemonsound/PokemonSoundPluginTest.java
@@ -1,9 +1,9 @@
-package com.example;
+package com.pokemonsound;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class PokemonSoundPluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
